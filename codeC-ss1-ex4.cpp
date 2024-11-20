ed54a532e9258d312511d3ec2b900c29dc1c3f34
hieutitz <nt.hieu616@gmail.com>
2024-11-20 22:46:18 +0700

    Add files via upload

---
+++ codeC-ss1-ex4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int chieu dai = 5;
+    int chu vi = 4 * chieu dai;
+    int dien tich = chieu dai * chieu dai;
+    printf("Canh cua hinh vuong la: %d\n", chieu dai);
+    printf("Chu vi cua hinh vuong la: %d\n", chu vi);
+    printf("Dien tich cua hinh vuong la: %d\n", dien tich);
+}
+
