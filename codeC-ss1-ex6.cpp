e7e6c824adec2658d294ab16bfcc103c59f98dda
hieutitz <nt.hieu616@gmail.com>
2024-11-20 22:46:38 +0700

    Add files via upload

---
+++ codeC-ss1-ex6.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    const float PI = 3.14;
+    float bkinh = 7.0;
+
+    float chu vi = 2 * PI * bkinh;
+    float dien tich = PI * bkinh * bkinh;
+
+    printf("Ban kinh: %.2f\n", bkinh);
+    printf("Chu vi hinh tron: %.2f\n", chu vi);
+    printf("Dien tich hinh tron: %.2f\n", dien tich;
+}
+
