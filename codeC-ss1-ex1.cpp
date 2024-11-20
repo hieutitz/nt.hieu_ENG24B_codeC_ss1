8ef46457024a2308eb0d04e3b4ff5f5b5f20c292
hieutitz <nt.hieu616@gmail.com>
2024-11-20 22:45:48 +0700

    Add files via upload

---
+++ codeC-ss1-ex1.cpp
@@ -0,0 +1,5 @@
+// khai bao va khoi tao cac bien voi cac kieu du lieu co ban trong C
+ int age = 18; // int dung de luu tru so nguyen
+ float height = 1.75; // float dung de luu so thuc co dau
+ double pi = 3.141592653589793; //douuble de chi tiet hon float
+ char class = 'B' //char de luu ky tu
