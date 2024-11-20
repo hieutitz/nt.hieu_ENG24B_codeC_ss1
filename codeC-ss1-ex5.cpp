17131841acb4662579b5ecd662249714975bfdc5
hieutitz <nt.hieu616@gmail.com>
2024-11-20 22:46:29 +0700

    Add files via upload

---
+++ codeC-ss1-ex5.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int dai = 8;
+    int rong = 5;
+    int chu vi = 2 * (dai + rong);
+    int dien tich = dai * rong;
+
+    printf("Chieu dai: %d\n", dai);
+    printf("Chieu rong: %d\n", rong);
+    printf("Chu vi: %d\n", chu vi);
+    printf("Dien tich: %d\n", dien tich);
+}
+
